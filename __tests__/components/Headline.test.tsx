2128d080a49c0fd9ba9a17870d09acdf7be11d41
hbrandt <h.brandt@steinberg.de>
2023-03-03 12:00:01 +0100

    test: Create headline component and tests for it

---
+++ __tests__/components/Headline.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Headline, { HeadlineTypes } from '../../src/components/Headline';
+
+describe('Headline', () => {
+  const headlineTypes: HeadlineTypes[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+  const headlineText = 'This is a headline';
+  const headlineClassName = 'headline';
+  const component = (
+    <Headline
+      headlineText={headlineText}
+      headlineType='h1'
+      headlineClass={headlineClassName}
+    />
+  );
+  it('redners given headline tag with given text', () => {
+    headlineTypes.forEach((item) => {
+      render(<Headline headlineText={headlineText} headlineType={item} />);
+      expect(screen.getByRole('heading').tagName).toBe(item.toUpperCase());
+      expect(screen.getByRole('heading')).toHaveTextContent(headlineText);
+      cleanup();
+    });
+  });
+  it('returns empty DOM element when no text is given', () => {
+    const { container } = render(
+      <Headline headlineText='' headlineType='h1' />
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
+  it('returns empty DOM element when no headline-type is given', () => {
+    const { container } = render(
+      <Headline headlineText='' headlineType={'' as HeadlineTypes} />
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
+  it('has className when given', () => {
+    render(component);
+    expect(screen.getByRole('heading')).toHaveClass(headlineClassName);
+  });
+  it('has no className when not given', () => {
+    render(<Headline headlineText={headlineText} headlineType={'h1'} />);
+    expect(screen.getByRole('heading')).not.toHaveClass(headlineClassName);
+  });
+  it('matches snapshot', () => {
+    const { container } = render(component);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/Headline.tsx
@@ -0,0 +1,30 @@
+export type HeadlineTypes = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+export type HeadlineProps = {
+  headlineText: string;
+  headlineType: HeadlineTypes;
+  headlineClass?: string;
+};
+
+const Headline = ({
+  headlineText,
+  headlineType,
+  headlineClass,
+}: HeadlineProps) => {
+  if (!headlineText || !headlineType) {
+    return <></>;
+  }
+
+  const headlineWithText = {
+    h1: <h1 className={headlineClass ?? ''}>{headlineText}</h1>,
+    h2: <h2 className={headlineClass ?? ''}>{headlineText}</h2>,
+    h3: <h3 className={headlineClass ?? ''}>{headlineText}</h3>,
+    h4: <h4 className={headlineClass ?? ''}>{headlineText}</h4>,
+    h5: <h5 className={headlineClass ?? ''}>{headlineText}</h5>,
+    h6: <h6 className={headlineClass ?? ''}>{headlineText}</h6>,
+  };
+
+  return headlineWithText[headlineType] || headlineWithText['h1'];
+};
+
+export default Headline;
